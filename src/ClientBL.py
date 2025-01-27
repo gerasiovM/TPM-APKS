4d4f4a33330e8b339f8a8250dc6630038bec0621
gerasiovM <maxim.gerasiov@gmail.com>
2025-01-27 22:54:10 +0200

    Changes to ClientBL
    - Added send_no_enc function which sends a message without utilising the fernet encryption
    
    - Added a static setup_session function which is used for creating basic auth_sessions for credential based authentication
    
    - Added an authenticate function which handles credential based authentication. Currently finished up to getting the secret from the encrypted payload.

--- src/ClientBL.py
+++ src/ClientBL.py
@@ -5,6 +5,12 @@ import cryptography.exceptions
 from cryptography import fernet
 from cryptography.hazmat.primitives import hmac, serialization
 from cryptography.hazmat.primitives.asymmetric import rsa
+from tpm2_pytss import ESAPI, TPMT_SYM_DEF, TPM2_ALG, TPMU_SYM_KEY_BITS, TPMU_SYM_MODE, TPM2_SE, TPMA_SESSION, \
+    TPM2B_ID_OBJECT, TPM2B_ENCRYPTED_SECRET, TSS2_Exception
+from tpm2_pytss.utils import NVReadEK, create_ek_template
+from tpm2_pytss.types import TPM2B_SENSITIVE_CREATE
+from tpm2_pytss.constants import ESYS_TR
+
 class ClientBL:
     def __init__(self):
         self._socket: socket.socket = None
@@ -14,6 +20,8 @@ class ClientBL:
         self._logged_in = False
         self._fernet: fernet.Fernet = None
         self._hmac_manager: hmac.HMAC = None
+        self._ek_handle: ESYS_TR = None
+        self._ak_handle: ESYS_TR = None
 
     def connect(self) -> bool:
         try:
@@ -51,6 +59,11 @@ class ClientBL:
             logging.error("[CLIENT_BL] Exception on send: {}".format(e))
             return False
 
+    def send_no_enc(self, data: bytes or str, data_type: str) -> bool:
+        if type(data) is str:
+            data = data.encode(Protocol.FORMAT)
+        self._p.send_bytes(self._socket, data_type, b"", data)
+
     def receive(self) -> bytes:
         if not self._socket:
             return b""
@@ -75,6 +88,45 @@ class ClientBL:
         except Exception as e:
             logging.error("[CLIENT_BL] Exception on receive: {}".format(e))
         return b""
+
+    @staticmethod
+    def setup_session(ectx, ek_handle):
+        sym = TPMT_SYM_DEF(algorithm=TPM2_ALG.XOR,
+                           keyBits=TPMU_SYM_KEY_BITS(exclusiveOr=TPM2_ALG.SHA256),
+                           mode=TPMU_SYM_MODE(aes=TPM2_ALG.CFB))
+        session = ectx.start_auth_session(tpm_key=ek_handle,
+                                          bind=ESYS_TR.NONE,
+                                          session_type=TPM2_SE.POLICY,
+                                          symmetric=sym,
+                                          auth_hash=TPM2_ALG.SHA256)
+        nonce = ectx.trsess_get_nonce_tpm(session)
+        expiration = -(10 * 365 * 24 * 60 * 60)
+        ectx.policy_secret(ESYS_TR.ENDORSEMENT, session, nonce, b"", b"", expiration)
+        ectx.trsess_set_attributes(session, TPMA_SESSION.ENCRYPT | TPMA_SESSION.DECRYPT)
+        return session
+
+    def authenticate(self):
+        try:
+            with ESAPI() as ectx:
+                nv_read = NVReadEK()
+                ek_cert, ek_template = create_ek_template("EK-RSA2048", nv_read)
+                self.ek_handle, ek_pub, _, _, _ = ectx.create_primary(TPM2B_SENSITIVE_CREATE(), ek_template, ESYS_TR.RH_ENDORSEMENT)
+                session = self.setup_session(ectx, self.ek_handle)
+                ak_priv, ak_pub, _, _, _ = ectx.create(self.ek_handle, in_sensitive=None, session1=session)
+                session = self.setup_session(ectx, self.ek_handle)
+                self.ak_handle = ectx.load(self.ek_handle, ak_priv, ak_pub, session1=session)
+                self.send(ek_pub.to_pem() + Protocol.DELIMITER + ak_pub.to_pem() + Protocol.DELIMITER + ek_cert, "AUTH")
+                response = self.receive()
+                while not response:
+                    response = self.receive()
+                credblob, secret = response.split(Protocol.DELIMITER)
+                session = self.setup_session(ectx, self.ek_handle)
+                certinfo = ectx.activate_credential(self.ak_handle, self.ek_handle, TPM2B_ID_OBJECT.unmarshal(credblob), TPM2B_ENCRYPTED_SECRET.unmarshal(secret), session2=session)[2:]
+                print(certinfo)
+        except TSS2_Exception as e:
+            logging.error("[CLIENT_BL] Exception on authenticate, confirm that the user has the permission to interact with the tpm {}".format(e))
+
+
 
     def login(self, login: str, password: str):
         login = Protocol.standardize(login[:20].encode(Protocol.FORMAT), Protocol.LOGIN_SIZE)
